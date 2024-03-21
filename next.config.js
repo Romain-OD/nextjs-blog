5b1b2d7d49b3a6b6bf2f76788c27cfcaaa0e5467
Romain O.D <31764259+Romain-OD@users.noreply.github.com>
2024-03-21 16:00:36 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",  // <=== enables static exports
   reactStrictMode: true,
-  swcMinify: true,
-  experimental: {
-    appDir: true,
-  },
 };
 
 module.exports = nextConfig;
